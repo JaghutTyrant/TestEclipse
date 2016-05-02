f05774bee1c8d7bf2fe4fe2f746032a7fae8dddb
jaghuttyrant <alrodperez@gmail.com>
2016-05-02 18:41:26 -0500

    18:41 commit

--- src/TestCommit.java
+++ src/TestCommit.java
@@ -1,9 +1,16 @@
-
+import java.util.*;
 public class TestCommit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test commiTest committ");
+		List<String> myList;
+		myList = new ArrayList<String>();
+		myList.add("new string");
+		for (String string : myList) {
+			System.out.println("This is my string: "+string);
+		}
+
 
 	}
 
